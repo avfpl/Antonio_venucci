eaf751e3ee7b5c5a892f67ba4a325d6d94e1d5df
aman-vishen <amanvishen46@gmail.com>
2023-07-27 03:40:31 +0530

    testing

--- mail.php
+++ mail.php
@@ -6,7 +6,7 @@
         $name = strip_tags(trim($_POST["name"]));
 				$name = str_replace(array("\r","\n"),array(" "," "),$name);
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $subject = trim($_POST["subject"]);
+        // $subject = trim($_POST["subject"]);
         $message = trim($_POST["message"]);
 
         // Check that data was sent to the mailer.
@@ -27,14 +27,14 @@
         // Build the email content.
         $email_content = "Name: $name\n";
         $email_content .= "Email: $email\n\n";
-        $email_content .= "Subject: $subject\n\n";
+        // $email_content .= "Subject: $subject\n\n";
         $email_content .= "Message:\n$message\n";
 
         // Build the email headers.
         $email_headers = "From: $name <$email>";
 
         // Send the email.
-        if (mail($recipient, $subject, $email_content, $email_headers)) {
+        if (mail($recipient, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
             echo "Thank You! Your message has been sent.";
