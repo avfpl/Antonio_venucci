acfc33663f16e8bba8ea9994c30a76b232846844
aman-vishen <amanvishen46@gmail.com>
2023-07-29 00:01:32 +0530

    test

--- mail.php
+++ mail.php
@@ -38,6 +38,7 @@
             // Set a 200 (okay) response code.
             http_response_code(200);
             echo "Thank You! Your message has been sent.";
+           
         } else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
