6593d5dcc24be9e4e942fa32f1a49a97f45e4085
aman-vishen <amanvishen46@gmail.com>
2023-07-31 02:11:49 +0530

    page1_repsoove

--- mail.php
+++ mail.php
@@ -13,7 +13,7 @@
         if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Set a 400 (bad request) response code and exit.
             http_response_code(400);
-            echo "Please complete the form and try again.";
+            die("Please complete the form and try again.");
             exit;
         }
 
@@ -42,13 +42,13 @@
         } else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
+            die("Oops! Something went wrong and we couldn't send your message.");
         }
 
     } else {
         // Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
+        die("There was a problem with your submission, please try again.");
     }
 
 ?>
